60ed5b0c858a03f84ef86e5675a7cd2d4ab70167
zamasker <zamasker@gmail.com>
2024-06-02 17:57:50 +0200

    Kolejny commit

---
+++ app/nowa/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Nowa() {
+
+    const title = "Inna strona";
+
+
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <div className="flex flex-col gap-y-3">
+                <h1 className="text-center text-5xl text-orange-400">{title}</h1>
+                <div className="text-center"> Jeszcze bez obrazka</div>
+            </div>
+        </div>
+    );
+} 
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+      <div className="h-screen flex items-center justify-center">
+        <div className="flex flex-col gap-y-3">
+          <h1 className="tracking-tight font-bold text-5xl text-orange-400">Przemysław Czajka</h1>
+          <p className="text-3xl text-center">Nr albumu: 119752</p>
+
+          <Link href="/nowa" className="text-xs font-bold">Przejdź do drugiej strony</Link>
+        </div>npm run build
+      </div>
+  );
+}
