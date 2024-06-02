a655ee57cc85c8e063ec967334c77c67b4861ad3
zamasker <zamasker@gmail.com>
2024-06-02 18:32:42 +0200

    Konczymy projekt

--- app/nowa/page.tsx
+++ app/nowa/page.tsx
@@ -1,14 +1,15 @@
 
 export default function nowa() {
 
-    const title = "Inna strona";
+    const title = "A oto jest sztuka";
 
 
     return (
         <div className="h-screen flex items-center justify-center">
             <div className="flex flex-col gap-y-3">
                 <h1 className="text-center text-5xl text-orange-400">{title}</h1>
-                <div className="text-center"> Jeszcze bez obrazka</div>
+                <div className="text-center"> Edward Hopper - Nighthawks </div>
+                <img src="nighthawks.jpg" alt="Nighthawks" className="w-640 h-350" />
             </div>
         </div>
     );
